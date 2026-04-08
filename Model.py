cbdbbd3e9fc1f06f4d11259f2631af1e9842e398
“vinu” <vinu.arasu@gmail.com>
2026-04-08 23:31:51 +0530

    Rebasing the MLops into wmark branch

--- Model.py
+++ Model.py
@@ -15,9 +15,10 @@ import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 import yaml
-
+import mlflow
 import warnings
 warnings.filterwarnings('ignore')
+from mlflow.models.signature import infer_signature
 
 # ==========================================
 # 1. Configuration & Setup
@@ -178,68 +179,81 @@ def reverse_transform(tensor):
 # ==========================================
 # 5. Training Engine
 # ==========================================
+import dagshub
+dagshub.init(repo_owner='vinu.arasu', repo_name='WaterMarkRemover', mlflow=True)
 
 if __name__ == '__main__':
-    train_dataset = WatermarkDataset(DATA_DIR, is_test=False)
-    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
 
-    model = ConditionalUNet().to(DEVICE)
-    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
-    criterion = nn.MSELoss()
-    history_loss = []
-    print("\n--- Starting Model Training ---")
+    #mlflow_uri = mlflow.set_tracking_uri("https://dagshub.com/vinu.arasu/WaterMarkRemover.mlflow")
+    mlflow.set_experiment('WMR')
+    with mlflow.start_run():
+        train_dataset = WatermarkDataset(DATA_DIR, is_test=False)
+        train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
 
-    for epoch in range(EPOCHS):
-        model.train()
-        running_loss = 0.0
-        pbar = tqdm(train_loader, desc=f"Epoch {epoch+1}/{EPOCHS}")
-        for watermarked, clean in pbar:
-            watermarked, clean = watermarked.to(DEVICE), clean.to(DEVICE)
-            t = torch.randint(0, TIMESTEPS, (clean.shape[0],), device=DEVICE).long()
-            x_t, noise = forward_diffusion(clean, t, DEVICE)
-            model_input = torch.cat([x_t, watermarked], dim=1)
-            predicted_noise = model(model_input, t)
-            loss = criterion(predicted_noise, noise)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item() * clean.size(0)
-            pbar.set_postfix({'Loss': f"{loss.item():.4f}"})
-        history_loss.append(running_loss / len(train_dataset))
+        model = ConditionalUNet().to(DEVICE)
+        optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+        criterion = nn.MSELoss()
+        history_loss = []
+        print("\n--- Starting Model Training ---")
+        mlflow.autolog()
+        for epoch in range(EPOCHS):
+            model.train()
+            running_loss = 0.0
+            pbar = tqdm(train_loader, desc=f"Epoch {epoch+1}/{EPOCHS}")
+            for watermarked, clean in pbar:
+                watermarked, clean = watermarked.to(DEVICE), clean.to(DEVICE)
+                t = torch.randint(0, TIMESTEPS, (clean.shape[0],), device=DEVICE).long()
+                x_t, noise = forward_diffusion(clean, t, DEVICE)
+                model_input = torch.cat([x_t, watermarked], dim=1)
+                predicted_noise = model(model_input, t)
+                loss = criterion(predicted_noise, noise)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                running_loss += loss.item() * clean.size(0)
+                pbar.set_postfix({'Loss': f"{loss.item():.4f}"})
+            history_loss.append(running_loss / len(train_dataset))
+            mlflow.log_metric("loss", running_loss / len(train_dataset), step=epoch)
+        mlflow.log_param("LR", LEARNING_RATE)
+        mlflow.log_param("Epoch", EPOCHS)
+        signature = infer_signature(model_input, predicted_noise)
+        saved_model = mlflow.pytorch.log_model(model, "Improved LR model", signature=signature)
 
-    torch.save(model.state_dict(), directory['model']+"/model.pth")
-    # Save Loss Graph
-    plt.figure(figsize=(10, 5))
-    plt.plot(range(1, EPOCHS + 1), history_loss, marker='o', color='crimson')
-    plt.title('Diffusion Training Loss Progression')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.grid(True)
-    plt.savefig('training_loss_progression.png')
+        torch.save(model.state_dict(), directory['model']+"/model.pth")
+        # Save Loss Graph
+        plt.figure(figsize=(10, 5))
+        plt.plot(range(1, EPOCHS + 1), history_loss, marker='o', color='crimson')
+        plt.title('Diffusion Training Loss Progression')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.grid(True)
+        plt.savefig('training_loss_progression.png')
 
-    # ==========================================
-    # 6. Inference & Sampling
-    # ==========================================
-    print("\n--- Generating Predictions ---")
-    test_dataset = WatermarkDataset(DATA_DIR, is_test=True)
-    test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
-    model.eval()
+        # ==========================================
+        # 6. Inference & Sampling
+        # ==========================================
+        if 0:
+            print("\n--- Generating Predictions ---")
+            test_dataset = WatermarkDataset(DATA_DIR, is_test=True)
+            test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+            #model.eval()
+            Pred_model = mlflow.pytorch.load_model(saved_model.model_uri)
 
-    with torch.no_grad():
-        for watermarked, filenames in tqdm(test_loader, desc="Inference"):
-            watermarked = watermarked.to(DEVICE)
-            b = watermarked.shape[0]
-            x = torch.randn((b, 3, IMG_SIZE, IMG_SIZE), device=DEVICE)
-            for i in reversed(range(TIMESTEPS)):
-                t = torch.full((b,), i, dtype=torch.long, device=DEVICE)
-                model_input = torch.cat([x, watermarked], dim=1)
-                pred_noise = model(model_input, t)
-                a_t = alpha[t].view(-1, 1, 1, 1).to(DEVICE)
-                a_bar_t = alpha_bar[t].view(-1, 1, 1, 1).to(DEVICE)
-                b_t = beta[t].view(-1, 1, 1, 1).to(DEVICE)
-                z = torch.randn_like(x) if i > 0 else 0
-                x = (1 / torch.sqrt(a_t)) * (x - ((1 - a_t) / torch.sqrt(1 - a_bar_t)) * pred_noise) + torch.sqrt(b_t) * z
-            for idx in range(b):
-                Image.fromarray(reverse_transform(x[idx])).save(os.path.join(OUTPUT_DIR, filenames[idx]))
+            with torch.no_grad():
+                for watermarked, filenames in tqdm(test_loader, desc="Inference"):
+                    watermarked = watermarked.to(DEVICE)
+                    b = watermarked.shape[0]
+                    x = torch.randn((b, 3, IMG_SIZE, IMG_SIZE), device=DEVICE)
+                    for i in reversed(range(TIMESTEPS)):
+                        t = torch.full((b,), i, dtype=torch.long, device=DEVICE)
+                        model_input = torch.cat([x, watermarked], dim=1)
+                        pred_noise = Pred_model(model_input, t)
+                        a_t = alpha[t].view(-1, 1, 1, 1).to(DEVICE)
+                        a_bar_t = alpha_bar[t].view(-1, 1, 1, 1).to(DEVICE)
+                        b_t = beta[t].view(-1, 1, 1, 1).to(DEVICE)
+                        z = torch.randn_like(x) if i > 0 else 0
+                        x = (1 / torch.sqrt(a_t)) * (x - ((1 - a_t) / torch.sqrt(1 - a_bar_t)) * pred_noise) + torch.sqrt(b_t) * z
+                    for idx in range(b):
+                        Image.fromarray(reverse_transform(x[idx])).save(os.path.join(OUTPUT_DIR, filenames[idx]))
 
-    
+        
